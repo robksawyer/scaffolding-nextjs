d7cf5f56332815eb495e1c66c7811f0c0ff90ee7
Rob Sawyer <robksawyer@gmail.com>
2021-07-11 01:31:02 -0500

    added src folder check

--- scaffold-component.js
+++ scaffold-component.js
@@ -2,6 +2,7 @@
  * @file bin/scaffold-component.js
  * Handles scaffolding a basic stateless component.
  */
+const fs = require('fs')
 const path = require('path')
 const minimist = require('minimist')
 const scaffoldComponent = require('./scaffolding/scaffold-component')
@@ -9,7 +10,14 @@ const scaffoldComponent = require('./scaffolding/scaffold-component')
 const constants = require('./constants')
 
 const PACKAGE_PATH = constants.getPackagePath()
-const SOURCE_DIR = constants.dirs().src
+let SOURCE_DIR = constants.dirs().src
+
+fs.access(path.resolve(PACKAGE_PATH, SOURCE_DIR, 'src'), function (error) {
+  if (!error) {
+    // Found src directory
+    SOURCE_DIR += '/src/'
+  }
+})
 
 module.exports = (function () {
   const argv = minimist(process.argv.slice(2))
